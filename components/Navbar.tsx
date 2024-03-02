75701223fb2921752093d7dfbf4af164c995059d
MuhammadUmar969 <muhammadumarazizkhan23@gmail.com>
2024-03-02 22:15:25 +0500

    public

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,10 +1,12 @@
-import React from 'react'
+import Link from "next/link"
 
 const Navbar = () => {
   return (
-    <div>
-      Nav
-    </div>
+    <nav className="border-2 border-red-500 flexBetween max-container padding-container relative z-20 py-5">
+      <Link href="/">
+
+      </Link>
+    </nav>
   )
 }
 
